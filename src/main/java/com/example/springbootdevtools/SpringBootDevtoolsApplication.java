8c422a0057bad491a2730b890e90e9ee424ae8cc
larger5 <354073878@qq.com>
2022-04-15 00:05:55 +0800

    init

---
+++ src/main/java/com/example/springbootdevtools/SpringBootDevtoolsApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootdevtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hongcunlin
+ */
+@SpringBootApplication
+public class SpringBootDevtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDevtoolsApplication.class, args);
+    }
+
+}
